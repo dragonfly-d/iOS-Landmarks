24aeddd8369921f01cc0dc1c248dc22d1efe9944
Dmitry <dragonfly_d@mail.ru>
2023-04-21 23:42:56 +0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dmitrii Cheishvili on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
